8b131d01e667ca695e5e941ad90b41a41767a19a
daooda <davinhoba9@outlook.fr>
2020-05-08 00:29:36 +0000

    adding week 8️⃣

--- week-8/main.py
+++ week-8/main.py
@@ -1,3 +1,10 @@
 # author @daoodaba975
 # GalsenDev
 
+string = input("Entrer votre phrase: ")
+
+string = string.replace('a', '*')
+string = string.replace('A', '*')
+
+print("Après modification: ")
+print(string)
